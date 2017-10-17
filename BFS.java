020820057865e93db7be4379a7d13c581b846a9f
Spas_Z <spaszahariev54@gmail.com>
2017-10-18 00:28:58 +0100

    Working BFS commit

--- BFS.java
+++ BFS.java
@@ -1,10 +1,10 @@
 import java.util.ArrayList;
-import java.util.PriorityQueue;
+import java.util.LinkedList;
 
 public class BFS {
 
     private static Node root;
-    private static PriorityQueue<Node> queue;
+    private static LinkedList<Node> queue;
 
     private static boolean isSolution = false;
 
@@ -15,26 +15,38 @@ public class BFS {
         //private Block blockB;
         //private Block blockC;
 
-        private ArrayList<Block> towerBlocks = new ArrayList<>(3);
-        //todo can change towerBlocks to arrayList for scalability
+        private ArrayList<Block> towerBlocks;
+
 
         private Node up;
         private Node right;
         private Node down;
         private Node left;
+
+        //optimise redundant moves
+        public boolean parentRight;
+        public boolean parentLeft;
+        public boolean parentUp;
+        public boolean parentDown;
+
+        public Node() {
+            towerBlocks = new ArrayList<>(3);
+            parentDown = parentUp = parentRight = parentLeft = false;
+        }
     }
 
-    public BFS () {
+    public BFS (Block rootAgent) {
         root = new Node();
-        queue = new PriorityQueue<>();
+        root.agent = rootAgent;
+        queue = new LinkedList<>();
     }
 
     //to add BlockA, BlockB, BlockC (can add blocks at same position but irrelevant right now)
-    private void insertBlock(Block newBlock) {
+    public void insertBlock(Block newBlock) {
         root.towerBlocks.add(newBlock);
     }
 
-    private void doBFS() {
+    public void doBFS() {
         queue.add(root);
         while (!isSolution)
         doBFS(queue.poll());
@@ -42,29 +54,49 @@ public class BFS {
 
     //todo optimise nodes to not go to their parents places
     private void doBFS(Node current) {
+
+        boolean reachedSolution = true;
+        for (Block i : current.towerBlocks) {
+            if (i.getCurrPos() != i.getGoalPos()) {
+                reachedSolution = false;
+                break;
+            }
+        }
+        if (reachedSolution) {
+            System.out.println("yey mofo");
+            isSolution = true;
+            //<3 lambda
+            current.towerBlocks.forEach(i -> System.out.println(i.getCurrPos()));
+            return;
+        }
+
         int agentPos = current.agent.getCurrPos();
 
         //is next to wall check
-        if (agentPos % 4 != 0) {
+        if (agentPos % 4 != 0 && !current.parentLeft) {
             current.left = new Node();
+            current.left.parentRight = true;
             makeSwitches(current.left, current, agentPos - 1, agentPos);
             queue.add(current.left);
         }
         //is next to wall check
-        if (agentPos % 4 != 3) {
+        if (agentPos % 4 != 3 && !current.parentRight) {
             current.right = new Node();
+            current.right.parentLeft = true;
             makeSwitches(current.right, current, agentPos + 1, agentPos);
             queue.add(current.right);
         }
         //is next to wall check
-        if (agentPos < 12) {
+        if (agentPos < 12 && !current.parentUp) {
             current.up = new Node();
+            current.up.parentDown = true;
             makeSwitches(current.up, current, agentPos + 4, agentPos);
             queue.add(current.up);
         }
         //is next to wall check
-        if (agentPos > 3) {
+        if (agentPos > 3 && !current.parentDown) {
             current.down = new Node();
+            current.down.parentUp = true;
             makeSwitches(current.down, current, agentPos - 4, agentPos);
             queue.add(current.down);
         }
@@ -78,7 +110,7 @@ public class BFS {
 
         for (Block i : parent.towerBlocks) {
             if (i.getCurrPos() == futureAgentPos) {
-                System.out.println("bug check: " + futureAgentPos + " moved to " + currentAgentPos);
+                System.out.println("bug check: agent " + currentAgentPos + " moved to " + futureAgentPos);
                 child.towerBlocks.add(new Block(currentAgentPos, i.getGoalPos()));
             } else {
                 child.towerBlocks.add(new Block(i.getCurrPos(), i.getGoalPos()));
--- PuzzleBoard.java
+++ PuzzleBoard.java
@@ -1,21 +1,27 @@
 public class PuzzleBoard {
 
     //private static int[][] grid;
-    private static Block blockA;
+    /*private static Block blockA;
     private static Block blockB;
     private static Block blockC;
-    private static Block agent;
+    private static Block agent;*/
 
     //constructor for default 4x4 A,B,C grid
-    public PuzzleBoard(){
+    public static void main(String args[]){
         //grid = new int[4][4];
-        blockA = new Block(0,9);
-        blockB = new Block(1,5);
-        blockC = new Block(2,1);
-        agent = new Block(3);
+        Block blockA = new Block(0,9);
+        Block blockB = new Block(1,5);
+        Block blockC = new Block(2,1);
+        Block agent = new Block(3);
+        BFS nodeBFS = new BFS(agent);
+        nodeBFS.insertBlock(blockA);
+        nodeBFS.insertBlock(blockB);
+        nodeBFS.insertBlock(blockC);
+        System.out.println("AT START: " + blockA.getCurrPos() + " " + blockB.getCurrPos() + " " + blockC.getCurrPos() + " " + agent.getCurrPos());
+        nodeBFS.doBFS();
     }
 
-    public Block getBlockA() {
+    /*public Block getBlockA() {
         return blockA;
     }
 
@@ -29,5 +35,5 @@ public class PuzzleBoard {
 
     public Block getAgent() {
         return agent;
-    }
+    }*/
 }
